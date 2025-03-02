f524d0e9e6638acfff171148952ad64ecf282127
victormilk <victor.milkcosta@gmail.com>
2025-03-02 21:50:04 +0000

    feat: :sparkles: Category validation class

--- src/category/domain/category.entity.ts
+++ src/category/domain/category.entity.ts
@@ -1,61 +1,67 @@
 import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
+import { CategoryValidatorFactory } from "./category.validator";
 
 export type CategoryConstructorProps = {
-    category_id?: Uuid;
-    name: string;
-    description?: string | null;
-    is_active?: boolean;
-    created_at?: Date;
-}
+  category_id?: Uuid;
+  name: string;
+  description?: string | null;
+  is_active?: boolean;
+  created_at?: Date;
+};
 
 export type CategoryCreateCommand = {
-    name: string;
-    description?: string | null;
-    is_active?: boolean;
-}
+  name: string;
+  description?: string | null;
+  is_active?: boolean;
+};
 
 export class Category {
-    category_id: Uuid;
-    name: string;
-    description: string | null;
-    is_active: boolean;
-    created_at: Date;
+  category_id: Uuid;
+  name: string;
+  description: string | null;
+  is_active: boolean;
+  created_at: Date;
 
-    constructor(props: CategoryConstructorProps) {
-        this.category_id = props.category_id ?? new Uuid();
-        this.name = props.name;
-        this.description =props.description ?? null;
-        this.is_active = props.is_active ?? true;
-        this.created_at = props.created_at ?? new Date();
-    }
+  constructor(props: CategoryConstructorProps) {
+    this.category_id = props.category_id ?? new Uuid();
+    this.name = props.name;
+    this.description = props.description ?? null;
+    this.is_active = props.is_active ?? true;
+    this.created_at = props.created_at ?? new Date();
+  }
 
-    static create(props: CategoryCreateCommand): Category {
-        return new Category(props);
-    }
+  static create(props: CategoryCreateCommand): Category {
+    return new Category(props);
+  }
 
-    changeName(name: string): void {
-        this.name = name;
-    }
+  changeName(name: string): void {
+    this.name = name;
+  }
 
-    changeDescription(description: string): void {
-        this.description = description;
-    }
+  changeDescription(description: string): void {
+    this.description = description;
+  }
 
-    activate(): void {
-        this.is_active = true;
-    }
+  activate(): void {
+    this.is_active = true;
+  }
 
-    deactivate(): void {
-        this.is_active = false;
-    }
+  deactivate(): void {
+    this.is_active = false;
+  }
 
-    toJSON() {
-        return {
-            category_id: this.category_id.id,
-            name: this.name,
-            description: this.description,
-            is_active: this.is_active,
-            created_at: this.created_at,
-        }
-    }
+  static validate(entity: Category) {
+    const validator = CategoryValidatorFactory.create();
+    return validator.validate(entity);
+  }
+
+  toJSON() {
+    return {
+      category_id: this.category_id.id,
+      name: this.name,
+      description: this.description,
+      is_active: this.is_active,
+      created_at: this.created_at,
+    };
+  }
 }
---
+++ src/category/domain/category.validator.ts
@@ -0,0 +1,40 @@
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from "class-validator";
+import { ClassValidatorFields } from "../../shared/domain/validators/class-validator-fields";
+import { Category } from "./category.entity";
+
+export class CategoryRules {
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  description: string | null;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  is_active: boolean;
+
+  constructor({ name, description, is_active }: Category) {
+    Object.assign(this, { name, description, is_active });
+  }
+}
+
+class CategoryValidator extends ClassValidatorFields<CategoryRules> {
+  validate(entity: Category) {
+    return super.validate(new CategoryRules(entity));
+  }
+}
+
+export class CategoryValidatorFactory {
+  static create(): CategoryValidator {
+    return new CategoryValidator();
+  }
+}
