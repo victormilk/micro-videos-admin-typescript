b7d176b2115424fdc1a16515de84550fd5dffef2
victormilk <victor.milkcosta@gmail.com>
2025-03-03 04:35:42 +0000

    feat: :sparkles: Create SearchableRepository interface

--- src/shared/infra/db/in-memory/in-memory.repository.spec.ts
+++ src/shared/infra/db/in-memory/in-memory.repository.spec.ts
@@ -1,7 +1,7 @@
-import { Entity } from "../../domain/entity";
-import { NotFoundError } from "../../domain/errors/not-found.error";
-import { ValueObject } from "../../domain/value-object";
-import { Uuid } from "../../domain/value-objects/uuid.vo";
+import { Entity } from "../../../domain/entity";
+import { NotFoundError } from "../../../domain/errors/not-found.error";
+import { ValueObject } from "../../../domain/value-object";
+import { Uuid } from "../../../domain/value-objects/uuid.vo";
 import { InMemoryRepository } from "./in-memory.repository";
 
 type StubEntityConstructorProps = {
