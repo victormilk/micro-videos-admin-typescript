ec82131baa68e2cb16dceb79927b3a1f16b221dd
victormilk <victor.milkcosta@gmail.com>
2025-03-03 03:27:43 +0000

    feat: :art: Create repository interface

--- src/category/domain/category.repository.ts
+++ src/category/domain/category.repository.ts
@@ -1,5 +1,5 @@
-import { Category } from './category.entity';
+import { IRepository } from "../../shared/domain/repository/repository-interface";
+import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
+import { Category } from "./category.entity";
 
-export interface CategoryRepository {
-
-}
+export interface ICategoryRepository extends IRepository<Category, Uuid> {}
